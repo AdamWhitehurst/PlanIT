37b810171f7c9f0965003ea3ff252323bd0d78b7
Mugdha Dhungana <mugdha@Mugdhas-MacBook-Pro.local>
2018-11-03 20:03:54 -0700

    Tasklist basics implemented

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+/*
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react-native';
+//import Tasks from './Elements/tasks'
 Amplify.configure(aws_exports);
+
 
 //Authenticate first
 const instructions = Platform.select({
@@ -13,15 +16,19 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+
+
+
 type Props = {};
 class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to PlanIt!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
+      
     );
   }
 }
@@ -31,17 +38,184 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#102372',
   },
   welcome: {
     fontSize: 20,
+    color: 'white',
     textAlign: 'center',
     margin: 10,
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: 'white',
     marginBottom: 5,
-  },
+  }, 
+
 });
+
+
+
 export default withAuthenticator(App);
+*/
+
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  AsyncStorage,
+  Button,
+  TextInput,
+  Keyboard,
+  Platform
+} from "react-native";
+
+const isAndroid = Platform.OS == "android";
+const viewPadding = 10;
+
+export default class TodoList extends Component {
+  state = {
+    tasks: [],
+    text: ""
+  };
+
+  changeTextHandler = text => {
+    this.setState({ text: text });
+  };
+
+  addTask = () => {
+    let notEmpty = this.state.text.trim().length > 0;
+
+    if (notEmpty) {
+      this.setState(
+        prevState => {
+          let { tasks, text } = prevState;
+          return {
+            tasks: tasks.concat({ key: tasks.length, text: text }),
+            text: ""
+          };
+        },
+        () => Tasks.save(this.state.tasks)
+      );
+    }
+  };
+
+  deleteTask = i => {
+    this.setState(
+      prevState => {
+        let tasks = prevState.tasks.slice();
+
+        tasks.splice(i, 1);
+
+        return { tasks: tasks };
+      },
+      () => Tasks.save(this.state.tasks)
+    );
+  };
+
+  componentDidMount() {
+    Keyboard.addListener(
+      isAndroid ? "keyboardDidShow" : "keyboardWillShow",
+      e => this.setState({ viewPadding: e.endCoordinates.height + viewPadding })
+    );
+
+    Keyboard.addListener(
+      isAndroid ? "keyboardDidHide" : "keyboardWillHide",
+      () => this.setState({ viewPadding: viewPadding })
+    );
+
+    Tasks.all(tasks => this.setState({ tasks: tasks || [] }));
+  }
+
+  render() {
+    return (
+      <View
+        style={[styles.container, { paddingBottom: this.state.viewPadding }]}
+      >
+        <FlatList
+          style={styles.list}
+          data={this.state.tasks}
+          renderItem={({ item, index }) =>
+            <View>
+              <View style={styles.listItemCont}>
+                <Text style={styles.listItem}>
+                  {item.text}
+                </Text>
+                <Button title="X" onPress={() => this.deleteTask(index)} />
+              </View>
+              <View style={styles.hr} />
+            </View>}
+        />
+        <TextInput
+          style={styles.textInput}
+          onChangeText={this.changeTextHandler}
+          onSubmitEditing={this.addTask}
+          value={this.state.text}
+          placeholder="Add Tasks"
+          returnKeyType="done"
+          returnKeyLabel="done"
+        />
+      </View>
+    );
+  }
+}
+
+let Tasks = {
+  convertToArrayOfObject(tasks, callback) {
+    return callback(
+      tasks ? tasks.split("||").map((task, i) => ({ key: i, text: task })) : []
+    );
+  },
+  convertToStringWithSeparators(tasks) {
+    return tasks.map(task => task.text).join("||");
+  },
+  all(callback) {
+    return AsyncStorage.getItem("TASKS", (err, tasks) =>
+      this.convertToArrayOfObject(tasks, callback)
+    );
+  },
+  save(tasks) {
+    AsyncStorage.setItem("TASKS", this.convertToStringWithSeparators(tasks));
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+    padding: viewPadding,
+    paddingTop: 20
+  },
+  list: {
+    width: "100%"
+  },
+  listItem: {
+    paddingTop: 30,
+    paddingBottom: 2,
+    fontSize: 18
+  },
+  hr: {
+    height: 1,
+    backgroundColor: "gray"
+  },
+  listItemCont: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  textInput: {
+    height: 40,
+    paddingRight: 10,
+    paddingLeft: 10,
+    borderColor: "gray",
+    borderWidth: isAndroid ? 0 : 1,
+    width: "100%"
+  }
+});
+
+AppRegistry.registerComponent("TodoList", () => TodoList);
---
+++ Elements/tasks.js
@@ -0,0 +1,32 @@
+
+
+
+class Tasks extends React.Component {
+    render() {
+      return(
+        <View style={StyleSheet.container}>
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.instructions}>To get started, edit App.js</Text>
+          <Text style={styles.instructions}>{instructions}</Text>
+        </View>
+      );
+    }
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    }
+  });
