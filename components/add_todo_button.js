63b60b345b408c2471814523166ea6ca9d573b45
Adam <Adam.Whitehurst@live.com>
2018-11-13 10:10:38 -0800

    Remove comment

--- components/add_todo_button.js
+++ components/add_todo_button.js
@@ -8,8 +8,7 @@ export default class AddToDoButton extends React.Component{
             <Container>        
                 <Fab
                     style={{ backgroundColor: '#5067FF' }}
-                    position="bottomRight"
-                    //onPress={() => console.log("Add todo button clicked") }>  
+                    position="bottomRight" 
                     onPress = {() => onAddNewToDo (show = true)}>          
                     <Icon name="add" />
                 </Fab>
