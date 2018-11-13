75a186d7490689c6da583623c8207ebf758f184b
Adam <Adam.Whitehurst@live.com>
2018-11-13 07:24:24 -0800

    Update

--- add_todo.js
+++ add_todo.js
@@ -1,54 +1,53 @@
-import React from 'react';
-import { Body,  Icon, CheckBox, ListItem } from 'native-base';
-import { Input, Button } from 'native-base';
+import React from "react";
+import { Body, Icon, CheckBox, ListItem } from "native-base";
+import { Input, Button } from "native-base";
 
-export default class AddToDo extends React.Component{   
-    constructor(props) {
-        super(props);        
-        const title = "";
-        const completed = false;
-        const createdAt = "";
+export default class AddToDo extends React.Component {
+  constructor(props) {
+    super(props);
+    const title = "";
+    const completed = false;
+    const createdAt = "";
 
-        this.state = {
-            title,
-            completed,
-            createdAt,
-        };
-    }
+    this.state = {
+      title,
+      completed,
+      createdAt
+    };
+  }
 
-    setStateUtil = (property, value) => {
-        this.setState({
-            [property]: value,
-        });
-    }
+  setStateUtil = (property, value) => {
+    this.setState({
+      [property]: value
+    });
+  };
 
-    render(){
-        //const completed = false;
-        const { completed, title } = this.state;
-        const { onPress, onCancel } = this.props;
-        return(                
-            <ListItem>
-                <CheckBox
-                    checked = { completed }
-                    //onPress = { () => console.log("set todo as completed") }
-                    onPress = { () => this.setStateUtil("completed", !completed) }
-                 />
-                <Body>
-                    <Input placeholder = "What needs to be done?"                    
-                        //onChangeText = { (txt) => console.log(txt) }                    
-                        //onSubmitEditing = { () => console.log("Text Submited") }                                                                
-                        onChangeText = { (txt) => this.setStateUtil("title", txt) }
-                        onSubmitEditing = { () => onPress( this.state ) }
-                    />
-                </Body>
-                <Button
-                    transparent
-                    //onPress = { () => console.log("put todo in trash") }
-                    onPress = { () => onCancel( show = false ) }
-                >
-                    <Icon name = { 'trash' } />                
-                </Button>
-            </ListItem> 
-        );
-    }
+  render() {
+    //const completed = false;
+    const { completed, title } = this.state;
+    const { onPress, onCancel } = this.props;
+    return (
+      <ListItem>
+        <CheckBox
+          checked={completed}
+          //onPress = { () => console.log("set todo as completed") }
+          onPress={() => this.setStateUtil("completed", !completed)}
+        />
+        <Body>
+          <Input
+            placeholder="What needs to be done?"
+            onChangeText={txt => this.setStateUtil("title", txt)}
+            onSubmitEditing={() => onPress(this.state)}
+          />
+        </Body>
+        <Button
+          transparent
+          //onPress = { () => console.log("put todo in trash") }
+          onPress={() => onCancel((show = false))}
+        >
+          <Icon name={"trash"} />
+        </Button>
+      </ListItem>
+    );
+  }
 }
