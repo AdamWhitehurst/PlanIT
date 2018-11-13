9b0a0f65abaf0196788352de447c51c9ffde2540
Adam <Adam.Whitehurst@live.com>
2018-11-13 10:14:56 -0800

    Refactor main_navigation, implement ToDoAll

--- main_navigation.js
+++ main_navigation.js
@@ -1,75 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {StyleSheet} from 'react-native';
-import { Container, Header, Title, Content, Body, Text, Icon } from 'native-base';
 import { TabNavigator, TabBarBottom } from 'react-navigation';
-import AddToDo from './components/add_todo';
-import AddToDoButton from './components/add_todo_button';
+import ToDoAll from './components/todo_all';
 
-class AllToDo extends React.Component {  
-  constructor(props) {
-    super(props);
-    this.state = {
-        new_todo: false,
-    };
-  }
-
-  saveToDoData = (todo) => {
-    this.addNewToDo(show = false);
-    console.log("Todo is: " + todo.title + " " + (todo.completed ? "completed!" : "not completed!"));
-  }
-
-  addNewToDo = (show) => {
-    this.setState({
-      new_todo: show
-    });
-  }
-
-  render() { 
-    const {new_todo} = this.state;
-
+class CompletedToDoTab extends React.Component {
+  render() {
     return (
-        <Container>
-          <Header><Body><Title>All</Title></Body></Header>         
-          <Content>  
-              {new_todo && 
-                <AddToDo 
-                  onPress = { this.saveToDoData }
-                  onCancel = { this.addNewToDo }
-                />
-              }
-            </Content>             
-            <AddToDoButton style={Theme.fab} onAddNewToDo = { this.addNewToDo }  />              
-        </Container>
+      <ToDoAll show_new_todo = { false } screen = "Completed" />
     );
   }
 }
 
-class ActiveToDo extends React.Component {
+class ActiveToDoTab extends React.Component {
   render() {
     return (
-      <Container>
-        <Header><Body><Title>Active</Title></Body></Header>
-        <Content></Content>    
-      </Container>
+      <ToDoAll show_new_todo = { false } screen = "Active" />
     );
   }
 }
 
-class CompletedToDo extends React.Component {
-  render() {
+class AllToDoTab extends React.Component {   
+  render() {    
     return (
-        <Container>
-          <Header><Body><Title>Completed</Title></Body></Header>
-          <Content></Content>
-        </Container>
+      <ToDoAll show_new_todo = { true } screen = "All" />
     );
   }
 }
 
 export default TabNavigator({
-  All: { screen: AllToDo },
-  Active: { screen: ActiveToDo },
-  Completed: { screen: CompletedToDo },
+  All: { screen: AllToDoTab },
+  Active: { screen: ActiveToDoTab },
+  Completed: { screen: CompletedToDoTab },
 },{
   navigationOptions: ({ navigation }) => ({
     tabBarIcon: ({ focused, tintColor }) => {
