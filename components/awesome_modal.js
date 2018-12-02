363dea2dee48ed6b4d6a5b9493fe734491ca9666
Adam <Adam.Whitehurst@live.com>
2018-12-02 14:12:03 -0800

    Fix my mess :100:

--- components/awesome_modal.js
+++ components/awesome_modal.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import { Container, Header, Content, Button, Text } from 'native-base';
 import { withNavigation } from 'react-navigation';
 export class BaseModalScreen extends Component {
+
+  saveToDoData = (todo) => {
+    let verified = this.verifyToDo(todo);
+    if (verified){
+      this.addNewToDo(show = false);
+      this.props.addTodo(todo); 
+    }   
+  }
+  
+  verifyToDo (todo) {
+    let verified = true;
+    // Empty title check
+    if (todo.title.trim() == '') {
+      Alert.alert ("Hey!","Add a title, dummy!", [{text: 'My bad.', onPress: ()=> {}}]);
+      verified = false;
+    }
+
+    return verified;
+  }
+
+
   render() {
     return (
       <Container>
